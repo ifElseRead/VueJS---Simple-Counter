1e63c29903aef315116ea361d7a799ab84f88211
ifElseRead <johnmurphy9@gmail.com>
2025-12-04 23:42:37 +0000

    Day 1: Completed Simple Counter (data, methods, v-on)

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+// 1. Define a reactive piece of data called 'count' with an initial value of 0.
+const count = ref(0);
+
+const increment = () => {
+  // We must access the value property of the ref object to change it
+  count.value++;
+};
+</script>
+
+<template>
+  <h1>Current Count: {{ count }}</h1>
+  <button @click="increment">Increment</button>
+</template>
+
+<style scoped>
+/* CSS goes here */
+</style>
